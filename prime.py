ad865c2f7796b3cb463be37f75a37edeaa30bbe8
krazysurjaa <44291967+krazysurjaa@users.noreply.github.com>
2019-02-17 10:19:25 -0800

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+def prime(n):
+    for i in range(2,n):
+        if n%i==0:
+            print(i)
+            print("Not a Prime No.")
+            return 0
+    print("Prime No.")
+    return 1
+
+n = int(input('enter number : '))
+prime(n)
